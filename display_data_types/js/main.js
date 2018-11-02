28011f2425b08e79e497ba8de251c077a331d604
Warnita Adams <warnita.adams.rsa@gmail.com>
2018-11-02 16:23:48 +0200

    added my challenge one an two in the correct folder

---
+++ display_data_types/js/main.js
@@ -0,0 +1,32 @@
+// Array
+    var listNumbers = [1, 2, 3, 4, 5];
+
+    console.log(listNumbers);
+
+// Object
+// String
+     var name = "warnita";
+
+// Number
+// Boolean
+function checkValue (a, x) {
+    if (a.includes(x)) {
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
+
+console.log(checkValue([1, 2, 3, 'love', 4, 5], 'love'));
+
+// Null
+// Undefined
+    var firstName;
+
+    console.log(firstName);
+// Date
+    var d = new Date();
+
+    console.log(new);
