64682f07133abe25ece763c3105d8cb425f9f3a5
Warnita Adams <warnita.adams.rsa@gmail.com>
2018-11-14 09:18:08 +0200

    adding challenge 2 and 6

---
+++ I_am_in_disarray/js/main.js
@@ -0,0 +1,3 @@
+var rainbow = ["red","orange","yellow","green","blue","purple","violet"];
+console.log(rainbow);
+console.log(rainbow[2]);
---
+++ If only I was_18/js/main.js
@@ -0,0 +1,6 @@
+var age = Number(prompt("Please enter your age:"));
+if (age <= 17) {
+    alert(" you are not allowed to view this site!");
+} else {
+    alert(" you are allowed to view this site!");
+}
