7873e6d7aa621feee02153efb2420da7629b9b76
Warnita Adams <warnita.adams.rsa@gmail.com>
2018-11-20 15:14:27 +0200

    created countdown timer with js

--- build_a_rainbow_2.0/js/main.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-{/* <button onclick="document.getElementById('red').style.background = 'red'">Red</button> */}
-=======
-<button onclick="document.getElementById('red').style.background = 'red'">Red</button>
->>>>>>> 463b8d1ee669dd78465174f36d51067401e41100
